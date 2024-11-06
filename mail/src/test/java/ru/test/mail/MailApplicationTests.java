750bf8777f117f70f055ddd364fc2c8b30faaffe
Phyllomedusa <matthew.vladimirovich@gmail.com>
2024-11-07 01:01:55 +0300

    Initial commit

---
+++ mail/src/test/java/ru/test/mail/MailApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
